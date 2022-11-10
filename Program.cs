738a89937d6a91d3edfac79ec1548b7ce133f7a8
SaulKaique <Saul.silva@etec.sp.gov.br>
2022-11-10 16:03:19 -0300

    so apenas com cores diferentes

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+
+Console.Write(".....Um-Dois-Tres-Pim.....\n");
+
+Console.ResetColor();
+
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+
+for (int n = 1; n <= 40; n++)
+    Console.Write(n % 4 == 0 ? "pim\n" : $"{n,2} ");
+
+
+Console.ResetColor();
